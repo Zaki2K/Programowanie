d69fb5f9e4f22d22b76057b37dbf1581d82fe180
Zaki2K <zakrzes1994@op.pl>
2016-10-17 15:52:04 +0200

    Pierwszy_Commit

---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car BMW = Car.Create(2.0, "Bmka");
+            Console.WriteLine(BMW.marka);
+            Console.WriteLine(BMW.pojSilnika);
+           
+            
+            Console.ReadKey();
+        }
+    }
+}
